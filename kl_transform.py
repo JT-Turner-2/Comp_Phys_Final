d4819ac626316326b481dcf72e4ddd5c3f48bc89
JT-Turner-2 <123335517+JT-Turner-2@users.noreply.github.com>
2023-05-09 14:44:49 -0400

    Update kl_transform.py

--- kl_transform.py
+++ kl_transform.py
@@ -1,3 +1,4 @@
-# the kl transform function should go here and make the other stuff in a seperate file (check this though)
-def kl_transform (yeet):
-  return yeet
+# the kl transform function should go here and make the other stuff in a seperate file 
+# need a function that creates the vector from the data
+def kl_transform (vector):
+  return vector
