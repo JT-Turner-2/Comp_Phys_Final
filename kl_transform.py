f155a033359c8b1aa6599c1ff24f689c844d35ed
JT-Turner-2 <123335517+JT-Turner-2@users.noreply.github.com>
2023-05-09 16:25:14 -0400

    Update kl_transform.py
    See comments. Should be complete now
    .

--- kl_transform.py
+++ kl_transform.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+# data needs to be in the form where catagories are the colums and galaxies are the rows. 
+#Code could be more efficent but I would rather make my own functions over using built ins. 
 def kl_transform(data):
     mat_t = data.T
     n = 0
@@ -20,7 +21,14 @@ def kl_transform(data):
                                  len(data) - 1)
             row.append(covariance)
         covariance_matrix.append(row)
-    return mean_vector, covariance_matrix
+
+    eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    eigenvalues = eigenvalues[sorted_indices]
+    eigenvectors = eigenvectors[:, sorted_indices]
+    transformed_data = np.dot(eigenvectors.T, data.T).T
+
+    return transformed_data
 
 SIGMA_1 = np.array([[4.0, 2.0,.60], [4.2, 2.1,.59],[3.9,2.0,.58],[4.3,2.1,.62],[4.1,2.2,.63]])
 func_test=kl_transform(SIGMA_1)
