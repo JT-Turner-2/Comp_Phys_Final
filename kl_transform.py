6cfbe24a41d4f07bb89fb9daa3b3e46e8567bed0
JT-Turner-2 <123335517+JT-Turner-2@users.noreply.github.com>
2023-05-11 07:45:19 -0400

    Update kl_transform.py
    actually sorts based on the evalues now

--- kl_transform.py
+++ kl_transform.py
@@ -1,4 +1,5 @@
-mport numpy as np
+import numpy as np
+import pdb
 
 def kl_transform(data):
     mat_t = data.T
@@ -11,6 +12,8 @@ def kl_transform(data):
 
 #covarience matrix time
     covariance_matrix = []
+    print("length of mean vector", len(mean_vector))
+    pdb.set_trace()
     for i in range(len(mean_vector)):
         row = []
         for j in range(len(mean_vector)):
@@ -22,21 +25,27 @@ def kl_transform(data):
         covariance_matrix.append(row)
 
     eigenvalues, eigenvectors = np.linalg.eig(covariance_matrix)
-    sorted_indices = np.argsort(eigenvalues)[::-1]
-    eigenvalues = eigenvalues[sorted_indices]
+    print("evalue_before_sort",eigenvalues)
+    c = np.savetxt('kl_unsorted_evectors_3.csv', (eigenvectors))
+    sorted_indices = np.argsort(-eigenvalues)
+    #print(sorted_indices)
+
+    eigenvalues_2 = np.sort(eigenvalues)[::-1]
+    print("evalue after sort",eigenvalues_2)
     eigenvectors = eigenvectors[:, sorted_indices]
+    c2=np.savetxt('kl_sorted_evectors_3.csv', (eigenvectors))
     transformed_data = np.dot(eigenvectors.T, data.T).T
 
     return transformed_data
 
-SIGMA_1 = np.array([[4.0, 2.0,.60], [4.2, 2.1,.59],[3.9,2.0,.58],[4.3,2.1,.62],[4.1,2.2,.63]])
-func_test=kl_transform(SIGMA_1)
-print("FUNC_TEST", func_test)
+#SIGMA_1 = np.array([[4.0, 2.0,.60], [4.2, 2.1,.59],[3.9,2.0,.58],[4.3,2.1,.62],[4.1,2.2,.63]])
+#func_test=kl_transform(SIGMA_1)
+#print("FUNC_TEST", func_test)
 
 
 #big_data=np.genfromtxt('/Users/jtturner/Dropbox/My Mac (Jacqueline’s MacBook Pro (3))/Downloads/304galaxiesreal.csv',delimiter=';'[0,:25])
-big_data=np.loadtxt('/Users/jtturner/Dropbox/My Mac (Jacqueline’s MacBook Pro (3))/Downloads/galaxy_edited_2.csv', delimiter=',', usecols=range(5))
-print(big_data)
+big_data=np.loadtxt('/Users/jtturner/Dropbox/My Mac (Jacqueline’s MacBook Pro (3))/Downloads/galaxy_edited_2.csv', delimiter=',')
+print(big_data,"big data")
 bd_test=kl_transform(big_data)
 print("process complete", bd_test)
-c = np.savetxt('kl_galaxies_v1.txt', (bd_test))
+c = np.savetxt('kl_galaxies_v3.txt', (bd_test))
